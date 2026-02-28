e007e1173096f91db5ae06501f21ef61d016a32d
Viktor Zivojinovic <viktorzivojinovic@Viktors-MacBook-Air.local>
2026-02-28 15:57:38 -0500

    first commit

---
+++ src/pages/Analytics/AnalyticsPage.jsx
@@ -0,0 +1,15 @@
+import "./AnalyticsPage.scss";
+import PageWrapper from "../../components/PageWrapper/PageWrapper";
+import BackArrowButton from "../../components/customButtons/BackArrowButton/BackArrowButton";
+import VerticalMenu from "../../components/VerticalMenu/VerticalMenu";
+
+const AnalyticsPage = ({ title, content }) => {
+
+    return (
+        <PageWrapper>
+            <VerticalMenu></VerticalMenu>
+        </PageWrapper>
+    );
+};
+
+export default AnalyticsPage;
